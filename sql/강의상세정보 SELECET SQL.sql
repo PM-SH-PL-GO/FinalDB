bd6abd0decccd9a4d01676c4e6df8a74ba075326
SDI998 <tta1424@naver.com>
2020-09-28 09:11:51 +0900

    sql

--- sql/강의상세정보 SELECET SQL.sql
+++ sql/강의상세정보 SELECET SQL.sql
@@ -1,4 +1,4 @@
-SELECT le.lecture_id, le.lecture_img, le.lecture_title, le.lecture_price, le.lecture_state
+SELECT le.lecture_id, le.lecture_img, le.lecture_title, le.lecture_category_id, le.lecture_price, le.lecture_state
 , le.lecture_start_dt, le.lecture_end_dt, le.lecture_max, le.lecture_min, le.lecture_current
 , ld.lecture_introduce, ld.lecture_curriculum, ld.lecture_prepared, ld.lecture_caution, ld.lecture_filename, ld.lecture_location
 , tu.tutor_id, tu.tutor_category_id, tu.tutor_nickname, tu.tutor_img, tu.tutor_score
