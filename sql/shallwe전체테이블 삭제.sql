e1d7d4db686ef3e56172884b3dd9ba4849715604
captic94 <seilu94@naver.com>
2020-10-09 14:43:05 +0900

    DB 최종 -> RDS에 다 올라가 있습니다

--- sql/shallwe전체테이블 삭제.sql
+++ sql/shallwe전체테이블 삭제.sql
@@ -1,6 +1,6 @@
 drop TABLE faq CASCADE CONSTRAINTS;
-drop TABLE freeboard CASCADE CONSTRAINTS;
-drop TABLE freereply CASCADE CONSTRAINTS;
+--drop TABLE freeboard CASCADE CONSTRAINTS;
+--drop TABLE freereply CASCADE CONSTRAINTS;
 drop table tutor_reject;
 drop table reject_category cascade constraints;
 drop table member_auth;
@@ -14,3 +14,7 @@ drop TABLE studyboard CASCADE CONSTRAINTS;
 drop TABLE studyreply CASCADE CONSTRAINTS;
 drop TABLE tutor CASCADE CONSTRAINTS;
 drop TABLE wishlist CASCADE CONSTRAINTS;
+--drop sequence faq_seq;
+--drop sequence lecture_seq;
+--drop sequence studyboard_seq;
+--drop sequence studyreply_seq;
